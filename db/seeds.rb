6dbec1d28c1424348d254538f6ff4a7334d7b551
Romain Sartorius <romsartorius@gmail.com>
2015-10-15 16:58:06 +0200

    added ingredients in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+Ingredient.destroy_all
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
 Ingredient.create(name: "rhum")
-Ingredient.create(name: "coca")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "coffee")
+Ingredient.create(name: "coca cola")
+Ingredient.create(name: "red bull")
 Ingredient.create(name: "soda")
+Ingredient.create(name: "ice")
