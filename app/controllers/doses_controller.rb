dc8b15e5192eb1a95731b531b397c95b17d507e5
Romain Sartorius <romsartorius@gmail.com>
2015-10-16 18:15:11 +0200

    fixed dose destroy

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,8 +16,8 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @dose = Dose.find(params[:dose_id])
-    @cocktail = @dose.cocktail
+    @dose = Dose.find(params[:id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
     redirect_to cocktail_path(@cocktail)
   end
